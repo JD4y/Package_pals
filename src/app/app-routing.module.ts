42416bd705a132d8051ef1001569e41ccaf55ec5
Jakub Dzien <jakub.dzien@b2tech.com>
2023-10-06 14:37:22 +0200

    Add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReceiverComponent } from './components/receiver/receiver.component';
+import { CollectorComponent } from './components/collector/collector.component';
+import { HistoryComponent } from './components/shared/history/history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'receiver', component: ReceiverComponent },
+  { path: 'collector', component: CollectorComponent },
+  { path: 'history', component: HistoryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
